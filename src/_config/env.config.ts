8de50162c4b9cb96e9ef6d9968302a6f962d5ed0
NoeOnDev <alxg5516@gmail.com>
2025-01-06 00:06:41 -0600

    Eliminar variables de configuración de la base de datos del archivo de entorno y ajustar la validación en la configuración del entorno

--- src/_config/env.config.ts
+++ src/_config/env.config.ts
@@ -2,11 +2,6 @@ process.loadEnvFile();
 
 type EnvConfig = {
   PORT: number;
-  DB_USER: string;
-  DB_PASSWORD: string;
-  DB_NAME: string;
-  DB_HOST: string;
-  DB_PORT: number;
 };
 
 const validateEnv = (): EnvConfig => {
@@ -16,27 +11,6 @@ const validateEnv = (): EnvConfig => {
     PORT: (val) => {
       const port = Number(val);
       if (isNaN(port)) throw new Error("PORT must be a number");
-      return port;
-    },
-    DB_USER: (val) => {
-      if (!val) throw new Error("DB_USER is required");
-      return val;
-    },
-    DB_PASSWORD: (val) => {
-      if (!val) throw new Error("DB_PASSWORD is required");
-      return val;
-    },
-    DB_NAME: (val) => {
-      if (!val) throw new Error("DB_NAME is required");
-      return val;
-    },
-    DB_HOST: (val) => {
-      if (!val) throw new Error("DB_HOST is required");
-      return val;
-    },
-    DB_PORT: (val) => {
-      const port = Number(val);
-      if (isNaN(port)) throw new Error("DB_PORT must be a number");
       return port;
     },
   };
@@ -61,12 +35,5 @@ const env = validateEnv();
 export const envConfig = {
   port: {
     PORT: env.PORT,
-  },
-  db: {
-    DB_USER: env.DB_USER,
-    DB_PASSWORD: env.DB_PASSWORD,
-    DB_NAME: env.DB_NAME,
-    DB_HOST: env.DB_HOST,
-    DB_PORT: env.DB_PORT,
   },
 };
